cf0b5d233f5c4381efaa16d1a4a34a8f0b23c5c0
蒲立望60263 <liwang.pu@going-link.com>
2023-09-07 21:25:18 +0800

    添加rc-app

---
+++ packages/my-rc-app/src/app/app.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.less';
+
+export function App() {
+  return (
+    <div className={styles['app']}>
+    </div>
+  );
+}
+
+export default App;
